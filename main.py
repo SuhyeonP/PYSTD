71c8789b8b7b3675068f31ceac7d98f8b14dfa6b
suhyeon <ahah12k@gmail.com>
2021-03-20 20:16:18 +0900

    std py

---
+++ main.py
@@ -0,0 +1,17 @@
+# one = input()
+# two = input()
+#
+# _min, _max =map(int, one.split())
+#
+# for n in map(int, two.split()):
+#     print(n)
+#     if n ==-999:
+#         break
+#     elif _min<=n<=_max:
+#         print('ok')
+#     else:
+#         print('error')
+#         break
+#
+
+print(int('6'))
